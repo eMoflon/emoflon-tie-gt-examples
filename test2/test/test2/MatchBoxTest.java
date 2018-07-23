b1a87303b85f8ce116514b03a921cef30793d785
David Giessing <david.giessing93@gmail.com>
2018-07-23 15:12:37 +0200

    seperated methods, tests for each method

--- test2/test/test2/MatchBoxTest.java
+++ test2/test/test2/MatchBoxTest.java
@@ -1,29 +1,96 @@
 package test2;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import org.junit.jupiter.api.Test;
 
 
 class MatchBoxTest {
 
+	private static final Test2Factory factory=Test2Factory.eINSTANCE;
 	@Test
-	void test() {
+	void matchPartitionPositive() {
 		Test2Factory factory=Test2Factory.eINSTANCE;
 		Box b= factory.createBox();
 		Partition p = factory.createPartition();
 		p.setBox(b);
+		//Preconditions
+		assertNull(p.getBox());
+		assertNull(b.getPartition());
 		b.matchABox();
+		//Postconditions
+		assertNotNull(b.getPartition());
+		assertNotNull(p.getBox());
 	}
 	
 	@Test
-	void testNegative() throws RuntimeException {
-		Test2Factory factory=Test2Factory.eINSTANCE;
+	void matchPartitionNegative() throws RuntimeException {
+		
 		Box b= factory.createBox();
-		Partition p = factory.createPartition();
 		try {
 			b.matchABox();
 		}
 		catch(RuntimeException e) {
-			System.out.println("Caught RuntimeException");
+			assert(true);
+			return;
+		}
+		assert(false);
+	}
+	
+	@Test
+	void removePartitionPositive() {
+		Box b= factory.createBox();
+		Partition p = factory.createPartition();
+		p.setBox(b);
+		//Preconditions
+		assertNotNull(b.getPartition());
+		assertNotNull(p.getBox());
+		b.removeAPartition();
+		//Postconditions
+		assertNull(p.getBox());
+		assertNull(b.getPartition());
+	}
+	
+	@Test
+	void removePartitionNegative() {
+		Box b= factory.createBox();
+		Partition p = factory.createPartition();
+		//Preconditions
+		assertNull(b.getPartition());
+		assertNull(p.getBox());
+		try {
+			b.removeAPartition();
+		}
+		catch(RuntimeException e) {
+			assert(true);
+			return;
+		}
+		assert(false);
+	}
+	
+	@Test
+	void addPartitionPositive() {
+		Box b= factory.createBox();
+		Partition p = factory.createPartition();
+		//Preconditions
+		assertNull(p.getBox());
+		assertNull(b.getPartition());
+		b.addAPartition();
+		//Postconditions
+		assertNotNull(b.getPartition());
+		assertNotNull(p.getBox());
+	}
+	
+	@Test
+	void addPartitionNegative() {
+		Box b= factory.createBox();
+		//Preconditions
+		assertNull(b.getPartition());
+		try {
+			b.addAPartition();
+		}
+		catch(RuntimeException e) {
 			assert(true);
 			return;
 		}
