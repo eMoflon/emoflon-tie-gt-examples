7eca37eedb45d5e551ab758d89bf3261e7136dd9
David Giessing <david.giessing93@gmail.com>
2018-07-30 12:20:56 +0200

    fixes for test

--- test2/test/test2/MatchBoxTest.java
+++ test2/test/test2/MatchBoxTest.java
@@ -15,8 +15,9 @@ class MatchBoxTest {
 		Test2Factory factory=Test2Factory.eINSTANCE;
 		Box b= factory.createBox();
 		b.setName("testbox");
-		b.setBoxWidthcm(1.1f);
+		b.setBoxWidthcm(1.1);
 		b.setIsAssignable(true);
+		b.setThisBoxId(1);
 		Partition p = factory.createPartition();
 		p.setBox(b);
 		b.setBoxSize(0);
@@ -24,7 +25,7 @@ class MatchBoxTest {
 		assertNotNull(p.getBox());
 		assertNotNull(b.getPartition());
 		assertTrue(b.getBoxSize()==0);
-		b.matchAPartition();
+		b.matchAPartition(1);
 		//Postconditions
 		assertNotNull(b.getPartition());
 		assertNotNull(p.getBox());
@@ -40,7 +41,7 @@ class MatchBoxTest {
 		Partition p = factory.createPartition();
 		p.setBox(b);
 		try {
-			b.matchAPartition();
+			b.matchAPartition(1);
 		}
 		catch(RuntimeException e) {
 			assertTrue(true);
@@ -53,11 +54,12 @@ class MatchBoxTest {
 		
 		Box b= factory.createBox();
 		b.setBoxSize(0);
+		b.setThisBoxId(1);
 		b.setName("this is not the box that you are looking for");
 		Partition p = factory.createPartition();
 		p.setBox(b);
 		try {
-			b.matchAPartition();
+			b.matchAPartition(1);
 		}
 		catch(RuntimeException e) {
 			assertTrue(true);
@@ -70,8 +72,9 @@ class MatchBoxTest {
 	void matchPartitionNegativeNoPartition() throws RuntimeException {
 		
 		Box b= factory.createBox();
+		b.setThisBoxId(1);
 		try {
-			b.matchAPartition();
+			b.matchAPartition(1);
 		}
 		catch(RuntimeException e) {
 			assertTrue(true);
@@ -131,6 +134,27 @@ class MatchBoxTest {
 		assertNull(b.getPartition());
 		try {
 			b.addAPartition();
+		}
+		catch(RuntimeException e) {
+			assertTrue(true);
+			return;
+		}
+		assertTrue(false);
+	}
+	
+	@Test
+	void matchPartitionNegativeParameter() throws RuntimeException {
+		
+		Box b= factory.createBox();
+		b.setName("testbox");
+		b.setBoxWidthcm(1.1f);
+		b.setIsAssignable(true);
+		b.setThisBoxId(1);
+		Partition p = factory.createPartition();
+		p.setBox(b);
+		b.setBoxSize(0);
+		try {
+			b.matchAPartition(0);
 		}
 		catch(RuntimeException e) {
 			assertTrue(true);
