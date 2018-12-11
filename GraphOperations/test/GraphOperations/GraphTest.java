3fdf5c841dda619966224ef349522035d13a610c
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-12-11 23:10:32 +0100

    Graph::clear now works

--- GraphOperations/test/GraphOperations/GraphTest.java
+++ GraphOperations/test/GraphOperations/GraphTest.java
@@ -1,6 +1,7 @@
 package GraphOperations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Optional;
 
