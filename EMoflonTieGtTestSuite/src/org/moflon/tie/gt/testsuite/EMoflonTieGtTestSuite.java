747b0ea90d3e764edacc8dfad2c3c1141c738f0b
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2019-02-11 18:55:34 +0100

    Add Sokoban tests to test suite

--- EMoflonTieGtTestSuite/src/org/moflon/tie/gt/testsuite/EMoflonTieGtTestSuite.java
+++ EMoflonTieGtTestSuite/src/org/moflon/tie/gt/testsuite/EMoflonTieGtTestSuite.java
@@ -7,13 +7,15 @@ import org.junit.runner.RunWith;
 import GraphOperations.ConstantUtilsTest;
 import GraphOperations.EIntContainerTest;
 import GraphOperations.GraphTest;
+import SokobanTests.SokobanTutorialAppendixTest;
+import SokobanTests.SokobanValidatorTest;
 import de.tud.es.dyve.model.NetworkTest;
 import de.tud.es.dyve.model.RootTest;
 import test2.MatchBoxTest;
 
 @RunWith(JUnitPlatform.class)
 @SelectClasses({ ConstantUtilsTest.class, EIntContainerTest.class, GraphTest.class, MatchBoxTest.class,
-		NetworkTest.class, RootTest.class })
+		NetworkTest.class, RootTest.class, SokobanValidatorTest.class, SokobanTutorialAppendixTest.class })
 public class EMoflonTieGtTestSuite {
 
 }
